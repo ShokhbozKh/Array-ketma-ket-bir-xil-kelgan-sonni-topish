55e89dc13ae137291e8fd894219e25dac65f9767
Shohboz <shohboztuit@gmail.com>
2023-07-27 10:41:03 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+            Random rd = new Random();
+
+            for(int i= 0; i<n; i++)
+            {
+                arr[i] = rd.Next(1, 8);
+                Console.Write(arr[i] +  " ");
+            }
+            Console.WriteLine();
+
+            for(int i = 1; i < n; i ++)
+            {
+                if (arr[i-1] == arr[i])
+                {
+                    Console.Write(arr[i] + " ");
+                }
+                
+             
+            }
+        }
+    }
+}
